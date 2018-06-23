fbd9a9986a0db59f0251631a60bb49eb08f8bbba
nc <charles.saternos@gmail.com>
2018-06-23 17:06:24 -0400

    ported more funcs

--- v2/source/app.d
+++ v2/source/app.d
@@ -1,25 +1,37 @@
 import opengl.gl4;
 import opengl.loader;
-//import opengl.d;
 import glfw3d;
+import gfm.math;
+import gfm.freeimage;
 import std.stdio;
 
-void clear_frame(float r, float g, float b) {
+void clearFrame(float r, float g, float b) {
     glClearColor(r, g, b, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-uint gen_vao() {
+struct VaoID {
+    uint id;
+    alias id this;
+}
+
+struct Mesh {
+    VaoID vao;
+    size_t verts;
+    // TODO: maybe track vbo ids?
+}
+
+VaoID genVAO() {
     uint id;
     glGenVertexArrays(1, &id);
     glBindVertexArray(id);
-    return id;
+    return VaoID(id);
 }
 
 // T - type of individual compoennt
 // N - number of components
-template gen_vbo(T, uint N) {
-    uint gen_vbo(uint i, T[] buf) {
+template genVBO(T, uint N) {
+    uint genVBO(uint i, T[] buf) {
         uint id;
         glGenBuffers(1, &id);
 
@@ -33,10 +45,51 @@ template gen_vbo(T, uint N) {
     }
 }
 
-void draw_points(uint vao, uint n) {
-    glBindVertexArray(vao);
-    glDrawArrays(GL_POINTS, 0, n);
+void drawPoints(Mesh m) {
+    glBindVertexArray(m.vao);
+    glDrawArrays(GL_POINTS, 0, cast(uint) m.verts);
     glBindVertexArray(0);
+}
+
+template drawLines(bool connected = false) {
+    void drawLines(Mesh m) {
+        glBindVertexArray(m.vao);
+        static if(connected) {
+            glDrawArrays(GL_LINE_STRIP, 0, cast(uint) m.verts);
+        } else {
+            glDrawArrays(GL_LINES, 0, cast(uint) m.verts);
+        }
+        glBindVertexArray(0);
+    }
+}
+
+void drawElements(Mesh m) {
+  glBindVertexArray(m.vao);
+  glDrawElements(GL_TRIANGLES, cast(uint) m.verts, GL_UNSIGNED_INT, null);
+  glBindVertexArray(0);
+}
+
+// TODO rewrite this sucker
+uint loadTexture(string fname) {
+    static int ntexs = 0;
+    assert(ntexs < 32);
+
+    uint id;
+    glGenTextures(1, &id);
+
+    int w, h;
+    glActiveTexture(GL_TEXTURE0 + ntexs++);
+    glBindTexture(GL_TEXTURE_2D, id);
+    auto img = new FIBitmap(new FreeImage(), fname);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.width(), img.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, img.data());
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // XXX not tracking texture id
+    return ntexs - 1;
 }
 
 void main() {
@@ -51,16 +104,24 @@ void main() {
 
     loadGL!(opengl.gl4);
 
-    uint vao = gen_vao();
-    gen_vbo!(float, 3)(0, [0,0,0,0.2,0.3,0]);
-    glPointSize(10.0);
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glDepthFunc(GL_LESS);
+    glPointSize(4);
+
+
+    Mesh m;
+    m.vao = genVAO();
+    m.verts = 2;
+    genVBO!(float, 3)(0, [0,0,0,0.2,0.3,0]);
     glBindVertexArray(0);
 
     while(!w.shouldClose()) {
         glfwPollEvents();
-        clear_frame(0, 0.1, 0);
+        clearFrame(0, 0.1, 0);
 
-        draw_points(vao, 2);
+        drawLines(m);
 
         w.swapBuffers();
     }
